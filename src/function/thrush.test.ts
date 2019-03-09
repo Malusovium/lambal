7c802580bad8936304db68b5560e984bdcc46e1c
Michiel Kamphuis <mfk@kamphuis.eu>
2019-03-09 22:35:44 +0100

    finished thrush fn

--- src/function/thrush.test.ts
+++ src/function/thrush.test.ts
@@ -1,8 +1,28 @@
 import test from 'ava'
 
+import { thrush } from './thrush'
+
 test
-( 'apply is a function'
+( 'thrush is a function'
 , (t) => {
-    t.fail()
+    const thrushType = typeof thrush
+    t.is(thrushType, 'function')
+  }
+)
+
+test
+( `thrush return's a function`
+, (t) => {
+    const thrushReturnType = typeof thrush
+    t.is(thrushReturnType, 'function')
+  }
+)
+
+test
+( 'thrush works properly'
+, (t) => {
+    const double = (x: number) => x * 2
+    const callWith10 = thrush (10)
+    t.is(callWith10 (double), 20)
   }
 )
