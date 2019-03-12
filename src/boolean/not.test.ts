3d48f7b3b48f07bbc4f87f5aa1d89fdd134d052d
Michiel Kamphuis <mfk@kamphuis.eu>
2019-03-12 19:30:17 +0100

    finished not fn

--- src/boolean/not.test.ts
+++ src/boolean/not.test.ts
@@ -1,8 +1,27 @@
 import test from 'ava'
 
+import { not } from './not'
+
 test
 ( 'not is a function'
 , (t) => {
-    t.fail()
+    const notType = typeof not
+    t.is(notType, 'function')
+  }
+)
+
+test
+( `not return's a boolean`
+, (t) => {
+    const notReturnType = typeof not (false)
+    t.is(notReturnType, 'boolean')
+  }
+)
+
+test
+( 'not works properly'
+, (t) => {
+    t.is(not (true), false)
+    t.is(not (false), true)
   }
 )
