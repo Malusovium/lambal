b30789b866f8e8e41be9542efd22b596f6f539d5
Michiel Kamphuis <mfk@kamphuis.eu>
2019-03-17 02:39:30 +0100

    finished times fn

---
+++ src/number/times.test.ts
@@ -0,0 +1,28 @@
+import test from 'ava'
+
+import { times } from './times'
+
+test
+( 'times is a function'
+, (t) => {
+    const timesType = typeof times
+    t.is(timesType, 'function')
+  }
+)
+
+test
+( `times return's a function`
+, (t) => {
+    const timesReturnType = typeof times (5)
+    t.is(timesReturnType, 'function')
+  }
+)
+
+test
+( 'times works properly'
+, (t) => {
+    t.is(times (10) (5), 50)
+    t.is(times (-5) (10), -50)
+    t.is(times (-1) (1), -1)
+  }
+)
