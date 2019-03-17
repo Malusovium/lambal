5b88de77a4659b19bcabee3a05321e90b014e03a
Michiel Kamphuis <mfk@kamphuis.eu>
2019-03-17 02:50:31 +0100

    finished power fn

---
+++ src/number/power.test.ts
@@ -0,0 +1,28 @@
+import test from 'ava'
+
+import { power } from './power'
+
+test
+( 'power is a function'
+, (t) => {
+    const powerType = typeof power
+    t.is(powerType, 'function')
+  }
+)
+
+test
+( `power return's a function`
+, (t) => {
+    const powerReturnType = typeof power (10)
+    t.is(powerReturnType, 'function')
+  }
+)
+
+test
+( 'power works properly'
+, (t) => {
+    t.is(power (3) (2), 8)
+    t.is(power (.5) (144), 12)
+    t.is(power (-.5) (9), 1/3)
+  }
+)
