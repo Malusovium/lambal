6d515698b834e9299fabe6b85dd8a557bbe28cf4
Michiel Kamphuis <mfk@kamphuis.eu>
2019-03-18 04:22:33 +0100

    finished replace fn

---
+++ src/string/replace.test.ts
@@ -0,0 +1,36 @@
+import test from 'ava'
+
+import { replace } from './replace'
+
+test
+( 'replace is a function'
+, (t) => {
+    const replaceType = typeof replace
+    t.is(replaceType, 'function')
+  }
+)
+
+test
+( `replace return's a function`
+, (t) => {
+    const replaceReturnType = typeof replace ('')
+    t.is(replaceReturnType, 'function')
+  }
+)
+
+test
+( `replace return's return's a function`
+, (t) => {
+    const replaceReturnReturnType = typeof replace ('') ('')
+    t.is(replaceReturnReturnType, 'function')
+  }
+)
+
+test
+( 'replace works properly'
+, (t) => {
+    t.is(replace ('foo') ('bar') (''), '')
+    t.is(replace ('foo') ('bar') ('bar'), 'foo')
+    t.is(replace ('foo') ('bar') ('bar bar ba b bar'), 'foo foo ba b foo')
+  }
+)
