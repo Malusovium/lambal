99b75e5070e4da0d4b77d328ea400bd1bca99f16
Michiel Kamphuis <mfk@kamphuis.eu>
2019-07-16 22:50:50 +0200

    good merge

--- src/boolean/equals.ts
+++ src/boolean/equals.ts
@@ -1,4 +1,5 @@
 import equal from 'fast-deep-equal'
+// const equal = (a: any, b: any) => true
 
 type Equals = <A> (a: A) => <B>(b: A | B) => boolean
 const equals: Equals = (a) => (b) => equal(a, b)
